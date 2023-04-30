bc81c015ce247deda68f2861566fd3f29ccd06ad
LucSky-Infosec <nguyencongluc.82@gmail.com>
2023-05-01 03:15:48 +0700

    final project

---
+++ app/src/main/java/com/example/finalproject/Model/Table.java
@@ -0,0 +1,53 @@
+package com.example.finalproject.Model;
+import java.util.Date;
+public class Table {
+    private int id;
+    private String TableName;
+    private Date DateCreated;
+    private Boolean Status;
+    public Table(){
+    }
+    public Table(int id, String tableName, Date dateCreated, Boolean status) {
+        this.id = id;
+        TableName = tableName;
+        this.DateCreated = dateCreated;
+        Status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTableName() {
+        return TableName;
+    }
+
+    public void setTableName(String tableName) {
+        TableName = tableName;
+    }
+
+    public Date getDateCreated() {
+        return DateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        DateCreated = dateCreated;
+    }
+
+    public Boolean getStatus() {
+        return Status;
+    }
+
+    public void setStatus(Boolean status) {
+        Status = status;
+    }
+    @Override
+    public String toString() {
+        return "Table" + "id=" + id + ", TableName=" + TableName + ", DateCreated=" + DateCreated + ", Status=" + Status + '}';
+    }
+
+}
